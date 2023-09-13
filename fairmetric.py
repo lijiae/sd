42658de2af0865f6b5f74bf44f93798fc16e57e4
Jiae <86452833+lijiae@users.noreply.github.com>
2023-09-13 10:56:35 +0800

    Update fairmetric.py

--- fairmetric.py
+++ fairmetric.py
@@ -3,6 +3,7 @@ import numpy as np
 import clip
 from PIL import Image
 import torch
+from tqdm import tqdm
 
 from torch.utils.data import DataLoader,Dataset
 
@@ -13,14 +14,17 @@ global model,preprocess
 
 class ImageData(Dataset):
     def __init__(self,imagepath_list,labels_list):
-        super(self,ImageData).__init__()
+        super(ImageData,self).__init__()
         self.name=imagepath_list
         self.label=labels_list
+        
+    def __len__(self):
+        return len(self.name)
     
     def __getitem__(self, index):
         imagepath=self.name[index]
         label=self.label[index]
-        image=preprocess(Image.open(imagepath)).unsqueeze(0)
+        image=preprocess(Image.open(imagepath))
         return image,label
         
 
@@ -30,14 +34,14 @@ class FairnessMetric():
         # model_name="RN50"
         # self.model,self.preprocess=clip.load(model_name,device=self.device)
         self.sensitive_words=["a female","a male","no human face"]
-        self.text=clip.tokenize(self.sensitive_words).to(self.device)
+        self.text=clip.tokenize(self.sensitive_words).to(device)
         
     def set_sensitive_attribute(self,attribute_list):
         self.sensitive_words=attribute_list
         
     def predict_attribute(self,image):
         with torch.no_grad():
-            logits_per_image, logits_per_text = self.model(image, self.text)
+            logits_per_image, logits_per_text = model(image, self.text)
             probs = logits_per_image.softmax(dim=-1).cpu().numpy()
         return probs
         
@@ -53,21 +57,22 @@ class FairnessMetric():
     
 fair=FairnessMetric()
 model,preprocess=clip.load(model_name,device=device)
-race_attributes=["an Asian","a Black", "an Indian", "a White","no human face"]
+race_attributes=["an Asian face","a Black face", "an Indian face", "a White man face","no human face"]
+fair.set_sensitive_attribute(race_attributes)
 # fair.gender_classification("/home/codes/diffusion/images/race-occupation-layer8-0.1/author/00003.png")
 # fair.gender_classification("/home/codes/diffusion/images/race-occupation-layer8-0.1/accountant/00000.png")
-csvpath=""
+csvpath="/home/codes/race-occupation-layer8-0.1.csv"
 csvfile=pd.read_csv(csvpath)
 imagelist=csvfile["image_path"]
-labellist=csvfile["occupations"]
+labellist=csvfile["label"]
 
 ds=ImageData(imagelist,labellist)
 results=[]
 dl=DataLoader(ds,32)
-for image,label in dl:
-    score=fair.predict_attribute(image)
+for image,label in tqdm(dl):
+    score=fair.predict_attribute(image.to(device))
     pre_id=np.argmax(score,axis=1)
-    attributes=fair.sensitive_words[pre_id]
+    attributes=[fair.sensitive_words[i] for i in pre_id]
     results=results+attributes
     
 df=pd.DataFrame({
